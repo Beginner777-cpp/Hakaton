ae6849ad047f2a8e6d7054e5802153f34684b571
test <test>
2020-11-30 12:31:36 +0500

    test

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@
 /*================================= burger ======================================*/
 var burger_icon = document.querySelector('.header_burger');
 var header_list = document.querySelectorAll('.header_list li');
-var header__content_top = document.querySelector('.header__content_top');
+var header__content_top = document.querySelector('.header_bottom');
 burger_icon.addEventListener('click', function () {
     if (!burger_icon.classList.contains('clicked')) {
         burger_icon.classList.add('clicked');
